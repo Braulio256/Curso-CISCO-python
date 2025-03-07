eedd25c902cc0ca57f02b9083e0ce0d14a351c76
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-06 20:32:04 -0600

    Programa 2 más usos de la función print()

---
+++ 0002_Mas usos de la funcion print().py
@@ -0,0 +1,3 @@
+print("\nWitsi Witsi Araña subió a su telaraña.")#Se puede añadir \n para hacer un salto de linea
+print()#Se puede añadir un print() para que la linea salga vacía
+print("Vino la lluvia y se la llevó.")
