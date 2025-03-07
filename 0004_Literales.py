e5f7247ae32e4a59859107d297b8f55d46556658
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-06 21:18:51 -0600

    Programa 4 Muestra del uso de las literales

---
+++ 0004_Literales.py
@@ -0,0 +1,5 @@
+print("Numero entero: ", 256)#Al no poner "" en el 256 se convierte en una literal en la que su valor es 256
+print("\nString: ", "256")#Al estar entre "" el 256 se convierte en un texto
+
+print("\nNumero octal: ",0o256)#Las literales pueden representarse en octal
+print("\nNumero hexadecimal: ",0x256)#O tambien en hexadecimal
