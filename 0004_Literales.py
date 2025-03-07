784a197e756b91c304eda270850eab12f274516c
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-06 21:30:59 -0600

    Programa 4 Se muestran diferentes tipos de literales asi como los enteros, flotantes, cadenas y distintas formas de colocar los numeros asi como los valores octales y hexadecimales

--- 0004_Literales.py
+++ 0004_Literales.py
@@ -1,5 +1,11 @@
-print("Numero entero: ", 256)#Al no poner "" en el 256 se convierte en una literal en la que su valor es 256
+print("Numero entero: ", 256)#Al no poner "" en el 256 se convierte en una literal de tipo entero
 print("\nString: ", "256")#Al estar entre "" el 256 se convierte en un texto
 
 print("\nNumero octal: ",0o256)#Las literales pueden representarse en octal
 print("\nNumero hexadecimal: ",0x256)#O tambien en hexadecimal
+
+print(15.024)#En las literales tambien se añaden los flotantes que son números con punto decimal
+print(15e-3)#Y tambien se pueden escribir utilizando la letra e
+
+print('Cadena')#Y tambien estan los String o cadenas que son las que van entre comillas simples o dobles
+print("")#Si se tienen solo las comillas sigue siendo una cadena
