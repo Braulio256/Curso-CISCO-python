53934f5e08b0dc42f867ef575de78456f309d486
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-06 21:07:33 -0600

    Programa 3 Palabras clave para la funcion print() Palabras clave (end y sep)

--- 0003_Palabras clave funcion print().py
+++ 0003_Palabras clave funcion print().py
@@ -2,3 +2,10 @@ print("Mi nombre es", "Braulio.", end=" ")
 #Esta la palabra clave "end" la cual arrastra el resultado del siguiente print() a la misma linea
 print("Braulio Alberto.")
 print("Buenas")#Pero si hay otro print() se debe agregar de nuevo la palabra clave
+
+print("\n\nMi", "nombre", "es", "Braulio", "Alberto.", sep="-_-")
+#Con la palabra clave sep va a insertar los caracteres que se introduzcan entre las comillas
+
+print("\n\nMi", "nombre", "es",sep=".", end=" ")
+print("Braulio", "Alberto.", sep="_", end="\n")
+#Las palabras clave se pueden utilizar al mismo tiempo
