d873dcbcf067d7cf00c2ccd7abef39ce574a6b1b
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-06 20:55:10 -0600

    Programa 3 Palabras clave para la funcion print()

---
+++ 0003_Palabras clave funcion print().py
@@ -0,0 +1,4 @@
+print("Mi nombre es", "Braulio.", end=" ")
+#Esta la palabra clave "end" la cual arrastra el resultado del siguiente print() a la misma linea
+print("Braulio Alberto.")
+print("Buenas")#Pero si hay otro print() se debe agregar de nuevo la palabra clave
