9eac54fa8bdec7bf8da649937abea28e98a1b4ae
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-15 15:43:25 -0600

    Programa 5 Operadores y su prioridad

--- 0005_Operadores.py
+++ 0005_Operadores.py
@@ -14,3 +14,18 @@ print(40/7)
 print(45//7)
 print(45%7)
 print(2**3)
+
+#Los operadores tienen prioridad
+#** operador de exponenciación
+#+, - (como operadores unarios estos tienen mas prioridad cuando estan acompañados del operador de exponenciación)
+#*, /, //, %
+#+, - (y por ultimo los operadores de suma y resta)
+
+#Ejemplo
+print(2**3+5)
+print(3**4-4)
+print(3*4/5)
+print(5+8/2)
+
+#Tambien se pueden usar parentesis para separar operaciones
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)
