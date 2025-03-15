899f6f493245d71904a6e7e733202c4e5d40b62c
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-15 15:01:41 -0600

    Programa 5 Operadores

---
+++ 0005_Operadores.py
@@ -0,0 +1,16 @@
+#+ operador de suma
+#- operador de resta
+#* operador de multiplicación
+#/ operador de división
+#// operador de división entera
+#% operador de residuo
+#** operador de exponenciación
+
+#Ejemplos de los operadores
+print(5+5)
+print(15-5)
+print(5*5)
+print(40/7)
+print(45//7)
+print(45%7)
+print(2**3)
