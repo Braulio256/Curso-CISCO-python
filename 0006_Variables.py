0b5931da2c1caaee1247188b25712b14cbb3fb32
Braulio Alberto Ortiz Reyes <143730671+Braulio256@users.noreply.github.com>
2025-03-16 15:34:00 -0600

    Programa 5 Operadores y su prioridad

---
+++ 0006_Variables.py
@@ -0,0 +1,15 @@
+#Una variable puede ser cualquier palabra siempre que no sea una palabra reservada
+#Ejemplo: if, while, for, break, etc.
+
+#Se pueden usar para cualquier cosa dentro del codigo
+#Ejemplo:
+nombre = "Braulio"
+edad = 25
+altura = 1.93
+print(nombre, edad, "años,", altura, "metros")
+
+#Tambien se pueden usar con los operadores
+a = 12
+b = 13
+c = (a ** 2 + b ** 2) ** 0.5
+print(c)
